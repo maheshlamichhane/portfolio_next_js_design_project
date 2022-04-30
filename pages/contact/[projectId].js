0ac490bb9b57f87322debe8696fe79c4bc8553a9
Software Engineer <softwarengineer007@gmail.com>
2022-04-30 18:18:16 +0545

    my first startup commit

---
+++ pages/contact/[projectId].js
@@ -0,0 +1,8 @@
+function DynamicContentPage() {
+    return (
+        <div>
+            <h1> Dynamic Content Page</h1>
+        </div>
+    );
+}
+export default DynamicContentPage;
---
+++ pages/work/index.js
@@ -0,0 +1,8 @@
+function WorkPage(){
+    return(
+        <div>
+            <h1>The Work Page</h1>
+        </div>
+    );
+}
+export default WorkPage;
